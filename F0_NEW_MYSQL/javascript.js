d58f07484b1f8c18526c93d19e333c0c086d3ae4
joaomaria04 <93591053+joaomaria04@users.noreply.github.com>
2021-11-04 08:48:14 +0000

    Delete javascript.js

--- F0_NEW_MYSQL/javascript.js
+++
@@ -1,8 +0,0 @@
-function ConfirmaApagar(id, nome) {
-    escolha = confirm("Deseja eliminar o registo com o id " + id + " com o nome " + nome + "?");
-if (escolha == 1) {
-    location = "eliminar.php?id_user=" + id;
-  } else {
-    location = "entrou.php";
-  }
-}
